04febfe5aa83e7a0906132bfc089a67a89be42c3
szilvike89 <szilvike@gmail.com>
2021-03-21 16:55:39 +0100

    9. feladat modositasa

--- fogyasztas.js
+++ fogyasztas.js
@@ -3,19 +3,19 @@ let km
 do
 {
     km = prompt("Hány kilométert mentél?")
-}while ((km <= 1000) && (km != NaN));
+}while ((km > 1000) && (km = NaN));
 
 let tankolas
 
 do{
     tankolas = prompt("Hány litert tankoltál?")
-}while (tankolas > 1 || tankolas < 50);
+}while (tankolas < 1 || tankolas > 50);
 
 let autok = {
     evjarat: 2020,
     automarka: "JEEP WRANGLER",
     fogyaszt: function(){
-        console.log(this.evjarat, " evjaratu ", this.automarka, " fogyasztasa: " + (tankolas / km) + "liter/km")
+        console.log(this.evjarat, " evjaratu ", this.automarka, " fogyasztasa: " + (tankolas*100 / km)/100 + " liter/km")
     }
 }
  autok.fogyaszt()
