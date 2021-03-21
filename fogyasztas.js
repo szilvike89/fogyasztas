1f37e146f10d8ebec058aebf89f22b32669d6690
szilvike89 <szilvike@gmail.com>
2021-03-21 16:15:43 +0100

    7. feladat

--- fogyasztas.js
+++ fogyasztas.js
@@ -5,3 +5,9 @@ do
     km = prompt("Hány kilométert mentél?")
 }while ((km <= 1000) && (km != NaN));
 
+let tankolas
+
+do{
+    tankolas = prompt("Hány litert tankoltál?")
+}while (tankolas > 1 || tankolas < 50);
+
